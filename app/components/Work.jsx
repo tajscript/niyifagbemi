6cad60a41164c34910fed660e927906222163b68
tajscript <ayomidejimoh247@gmail.com>
2023-10-05 23:30:57 +0100

    first animation

--- app/components/Work.jsx
+++ app/components/Work.jsx
@@ -4,12 +4,36 @@ import Link from "next/link"
 import WorkStyles from '../styles/work.module.css'
 import { AiOutlineArrowLeft, AiOutlineArrowRight } from 'react-icons/ai'
 import { gsap } from "gsap"
-import { useLayoutEffect, useRef } from "react"
+import { useLayoutEffect, useRef, useEffect, useState } from "react"
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger)
 
 const Work = () => {
   const workBgRef = useRef()
+  const [currentVideoIndex, setCurrentVideoIndex] = useState(0)
+  const [showTitle, setShowTitle] = useState(true)
+
+  const videos = [
+    { src: '/video2.mp4', title: 'Video 1 Title' },
+    { src: '/video2.mp4', title: 'Video 2 Title' },
+    { src: '/video2.mp4', title: 'Video 3 Title' },
+    { src: '/video2.mp4', title: 'Video 4 Title' },
+    { src: '/niyifagbemi.mp4', title: 'Video 5 Title' },
+    { src: '/video2.mp4', title: 'Video 6 Title' },
+    { src: '/video2.mp4', title: 'Video 7 Title' },
+    { src: '/video2.mp4', title: 'Video 8 Title' },
+    { src: '/video2.mp4', title: 'Video 9 Title' },
+    { src: '/niyifagbemi.mp4', title: 'Video 10 Title' },
+  ]
+
+  const handlePrevClick = () => {
+    setCurrentVideoIndex((prevIndex) => (prevIndex > 0 ? prevIndex - 1 : videos.length - 1))
+  }
+
+  const handleNextClick = () => {
+    setCurrentVideoIndex((prevIndex) => (prevIndex === videos.length - 1 ? 0 : prevIndex + 1))
+      }
+
 
   useLayoutEffect(() => {
 
@@ -46,69 +70,27 @@ const Work = () => {
         <div className={WorkStyles.work__container}>
         <div className={WorkStyles.work__video__wrapper}>
           <div className={WorkStyles.work__videos} ref={workBgRef}>
-            <div className={WorkStyles.work__video}>
-              <video autoPlay playsInline loop muted>
-                <source src="/video2.mp4" type="video/mp4" />
-              </video>
-            </div>
-
-            <div className={WorkStyles.work__video}>
-              <video autoPlay playsInline loop muted>
-                <source src="/video2.mp4" type="video/mp4" />
-              </video>
-            </div>
-
-            <div className={WorkStyles.work__video}>
-              <video autoPlay playsInline loop muted>
-                <source src="/video2.mp4" type="video/mp4" />
-              </video>
-            </div>
-
-            <div className={WorkStyles.work__video}>
-              <video autoPlay playsInline loop muted>
-                <source src="/video2.mp4" type="video/mp4" />
-              </video>
-            </div>
-
-            <div className={WorkStyles.work__video}>
-              <video autoPlay playsInline loop muted>
-                <source src="/video2.mp4" type="video/mp4" />
-              </video>
-            </div>
-
-            <div className={WorkStyles.work__video}>
-              <video autoPlay playsInline loop muted>
-                <source src="/video2.mp4" type="video/mp4" />
-              </video>
-            </div>
-
-            <div className={WorkStyles.work__video}>
-              <video autoPlay playsInline loop muted>
-                <source src="/video2.mp4" type="video/mp4" />
-              </video>
-            </div>
-
-            <div className={WorkStyles.work__video}>
-              <video autoPlay playsInline loop muted>
-                <source src="/video2.mp4" type="video/mp4" />
-              </video>
-            </div>
-
-            <div className={WorkStyles.work__video}>
-              <video autoPlay playsInline loop muted>
-                <source src="/video2.mp4" type="video/mp4" />
-              </video>
-            </div>
+            {videos.map((video, index) => (
+              <div
+                className={WorkStyles.work__video}
+                key={index}
+                >
+                  <video controls autoPlay playsInline loop muted={index !== currentVideoIndex}>
+                    <source src={video.src} type="video/mp4"/>
+                  </video>
+              </div>
+            ))
+            }
           </div>
 
           <div className={WorkStyles.work__video__title}>
-          <button className={WorkStyles.work__arrow}>
+          <button className={WorkStyles.work__arrow} onClick={handlePrevClick}>
           <AiOutlineArrowLeft />
           </button>
           
-          <h4>Video Title</h4>
+          {showTitle && <h4>{videos[currentVideoIndex].title}</h4>}
           
-          <button className={WorkStyles.work__arrow}>
+          <button className={WorkStyles.work__arrow} onClick={handleNextClick}>
           <AiOutlineArrowRight />
           </button>
           </div>
